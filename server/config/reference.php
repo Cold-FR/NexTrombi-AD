b2242a65f6b81ee2b45a6bc48cf2f0a820fb0cea
Cold-FR <david.clement5106@gmail.com>
2026-03-09 16:52:29 +0100

    feat(tests): integrate DAMA Doctrine Test Bundle for improved testing
    * Added DAMA\DoctrineTestBundle to enhance database testing capabilities.
    * Updated composer.json to include the new bundle as a development dependency.
    * Configured PHPUnit to utilize the new bundle's extension.
    * Created dama_doctrine_test_bundle.yaml for bundle-specific settings.

--- server/config/reference.php
+++ server/config/reference.php
@@ -1467,6 +1467,12 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *         },
  *     }>,
  * }
+ * @psalm-type DamaDoctrineTestConfig = array{
+ *     enable_static_connection?: mixed, // Default: true
+ *     enable_static_meta_data_cache?: bool|Param, // Default: true
+ *     enable_static_query_cache?: bool|Param, // Default: true
+ *     connection_keys?: list<mixed>,
+ * }
  * @psalm-type ConfigType = array{
  *     imports?: ImportsConfig,
  *     parameters?: ParametersConfig,
@@ -1514,6 +1520,7 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *         security?: SecurityConfig,
  *         lexik_jwt_authentication?: LexikJwtAuthenticationConfig,
  *         monolog?: MonologConfig,
+ *         dama_doctrine_test?: DamaDoctrineTestConfig,
  *     },
  *     ...<string, ExtensionType|array{ // extra keys must follow the when@%env% pattern or match an extension alias
  *         imports?: ImportsConfig,
