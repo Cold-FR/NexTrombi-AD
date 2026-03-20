8e710b78d21752a2aca38a354244e2e66d6a6af7
Cold-FR <david.clement5106@gmail.com>
2026-03-20 16:51:14 +0100

    feat(db): add migration for user_ignore table
    * Create user_ignore table with id and username columns
    * Remove unused example methods from UserIgnoreRepository

--- server/src/Repository/UserIgnoreRepository.php
+++ server/src/Repository/UserIgnoreRepository.php
@@ -15,29 +15,4 @@ class UserIgnoreRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, UserIgnore::class);
     }
-
-    //    /**
-    //     * @return UserIgnore[] Returns an array of UserIgnore objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('u')
-    //            ->andWhere('u.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('u.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?UserIgnore
-    //    {
-    //        return $this->createQueryBuilder('u')
-    //            ->andWhere('u.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
